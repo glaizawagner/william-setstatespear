91c5023b50fb67e8f10e3af9e7625f4dbec81482
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-14 22:39:24 -0700

    2-poem page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
+import { Route, Link  } from 'react-router-dom';
 import PoemListPage from './PoemListPage';
+import PoemPage from './PoemPage';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +11,8 @@ class App extends Component {
     return (
       <div className='App'>
         <nav>
-          <a href='/'>Poem List</a>
+          {/* <a href='/'>Poem List</a> */}
+          <Link href='/'>Poem List</Link>
         </nav>
         <header>
           <h1>William Setstatespear</h1>
@@ -16,6 +20,15 @@ class App extends Component {
         <main>
           {/* <PoemListPage /> */}
           <Route exact path='/' component ={PoemListPage}/>
+          {/* For Poem Page, an anonymous component that logs its route match prop */}
+          <Route path='/poem/:poemId'
+          //   component={(props) => {
+          //   console.log(props.match)
+          //   return <div />
+          // }} 
+            component={PoemPage}
+          />
+
         </main>
         <footer>
           <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
